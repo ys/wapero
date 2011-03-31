00cfccf9cbb6aee3a56ffea4c2b3e0b2427eb46e
eatcpcks <yannick.schutz@gmail.com>
2011-03-31 21:41:10 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wapero::Application.config.secret_token = '935d89c586a71bc264e37a0dee7ab3db42b3e78db49e4d9483a54cd4ebe334ab7f1259970e6f1f260661d7c8e72ce3836f07e18b5382e952dbecf110debfaca4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wapero::Application.config.session_store :cookie_store, :key => '_wapero_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wapero::Application.config.session_store :active_record_store
