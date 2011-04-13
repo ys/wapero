73d0d544d030e2bf6496ad97dd1fffd2a5bed74e
Hugues Lismonde <hugues@epicagency.net>
2011-04-13 20:14:28 +0200

    Moving to scss

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Sass::Plugin.options[:style] = :expanded
+Sass::Plugin.options[:line_numbers] = true
+
 # Initialize the rails application
 Wapero::Application.initialize!
